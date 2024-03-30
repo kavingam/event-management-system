720b47f6a2964cecb5ca90e5a1018b82d3ca66ac
kavingam <bhaitigam71@gmail.com>
2024-03-31 01:12:13 +0530

    update

--- about.php
+++ about.php
@@ -56,8 +56,16 @@
             <div class="col-sm-6">
             <div class="card text-end border-secondary h-100">
                 <div class="card-body">
-                        <h5 class="text-start card-title">Special title treatment</h5>
-                        <p class="text-start card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <h5 class="text-start card-title">Venue Selection and Booking: </h5>
+                    <p class="text-start card-text">Helping clients find and secure suitable venues for their events based on budget, location, capacity, and amenities.</p>
+                    <h5 class="text-start">Event Planning and Coordination: </h5>
+                    <p class="text-start">Assisting clients with all aspects of event planning, including creating timelines, developing event concepts, coordinating logistics, and managing vendors.</p>
+                    <h5 class="text-start">Budget Management:</h5>
+                    <p class="text-start">Helping clients develop and manage event budgets, including cost estimation, expense tracking, and financial reporting.</p>
+                    <h5 class="text-start"></h5>
+                    <p class="text-start"></p>
+                    <h5 class="text-start"></h5>
+                    <p class="text-start"></p>
                 </div>
             </div>                    
             </div>
@@ -102,22 +110,32 @@
             <div class="col-sm-6">
             <div class="card text-end border-secondary h-100">
                 <div class="card-body">
-                        <h5 class="text-start card-title">Special title treatment</h5>
-                        <p class="text-start card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <h5 class="text-start card-title">Vendor Management:</h5>
+                    <p class="text-start card-text">Sourcing and managing vendors such as caterers, decorators, audiovisual providers, photographers, and entertainment professionals.</p>
+                    <h5 class="text-start">Registration and Ticketing:</h5>
+                    <p class="text-start"> Setting up online registration systems, managing attendee registrations, and handling ticket sales for events.</p>
+                    <h5 class="text-start">Event Marketing and Promotion:</h5>
+                    <p class="text-start"> Developing marketing strategies, creating promotional materials, and managing social media campaigns to attract attendees and increase event visibility.</p>
                 </div>
             </div>                     
             </div>
             <div class="col-sm-6">
             <div class="card text-end border-secondary h-100">
                 <div class="card-body">
-                        <h5 class="text-start card-title">Special title treatment</h5>
-                        <p class="text-start card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <h5 class="text-start card-title">On-Site Event Management:</h5>
+                    <p class="text-start card-text"> Overseeing all aspects of event execution on the day of the event, including setup, coordination of vendors, managing staff and volunteers, and handling any issues or emergencies that may arise.</p>
+                    <h5 class="text-start">Attendee Management:</h5>
+                    <p class="text-start"> Providing support for attendee communication, including sending invitations, managing RSVPs, and providing information and assistance to attendees before and during the event.</p>
+                    <h5 class="text-start">Audiovisual Production:</h5>
+                    <p class="text-start"> Providing audiovisual equipment rental, setup, and technical support for presentations, performances, and other event elements.</p>
+                    <h5 class="text-start">Post-Event Evaluation: </h5>
+                    <p class="text-start">Conducting post-event evaluations to assess the success of the event, gather feedback from attendees, and identify areas for improvement for future events.</p>
                 </div>
             </div>                     
             </div>
             <div class="col-sm-6">
             <div class="card border-secondary h-100">
-                <img src="assets/img/slide-0.jpg" class="card-img-top" alt="...">
+                <img src="assets/img/slide-1.jpg" class="card-img-top" alt="...">
             </div>                   
             </div>
             <div class="col-sm-12">
@@ -125,15 +143,31 @@
                 <div class="row ">
                     <div class="col-sm-4">
                     <div class="card border-secondary h-100">
-                        <img src="assets/img/slide-0.jpg" class="card-img-top" alt="...">
+                        <img src="assets/wedding/Wedding_Venues-0.png" class="card-img-top" alt="...">
+                        <img src="assets/wedding/Wedding_Venues-1.png" class="card-img-top" alt="...">
                     </div>                    
                     </div>
                     <div class="col">
-                    <div class="card text-end border-secondary h-100">
-                        <div class="card-body bg-secondary">
-                            <h5 class="text-start card-title">Special title treatment</h5>
-                            <p class="text-start card-text">With supporting text below as a natural lead-in to additional content.</p>
-                            <a href="#" class="btn btn-primary">Learn more</a>
+                    <div class="card text-start  border-secondary h-100">
+                        <div class="card-body">
+                            <h5 class="text-start card-title">Theme: Love in Every Detail </h5>
+                            <p class="text-start card-text">At KHOT we believe that love is not just an emotion—it's a guiding principle that infuses every aspect of our event management approach. From intimate weddings to grand celebrations, our team is dedicated to curating unforgettable experiences that are filled with love, passion, and attention to detail.</p>
+                            <h5 class="text-start card-title">Personalized Planning:</h5><p>
+                            We understand that every love story is unique, and we take the time to get to know our clients and their vision for their special day. Our personalized planning approach ensures that every detail reflects the couple's love story, personality, and style. From venue selection to décor design, we tailor every aspect of the event to create a one-of-a-kind experience that captures the essence of their love.</p>
+
+                            <h5 class="text-start card-title">Heartfelt Connections:</h5><p>
+                            Building meaningful connections with our clients is at the heart of what we do. We believe in fostering open communication, trust, and collaboration throughout the planning process, so that our clients feel supported, heard, and valued every step of the way. By understanding their needs, dreams, and aspirations, we can create events that are truly reflective of their love and commitment.</p>
+
+                            <h5 class="text-start card-title">Thoughtful Execution:</h5><p>
+                            We believe that love is in the details, and we go above and beyond to ensure that every element of the event is thoughtfully curated and executed with precision and care. From meticulously planned timelines to seamless coordination of vendors and logistics, we take care of every aspect of the event so that our clients can relax and enjoy their special day surrounded by love and joy.</p>
+
+                            <h5 class="text-start card-title">Creating Lasting Memories:</h5><p>
+                            At [Your Event Management Company], we are passionate about creating unforgettable memories that will be cherished for a lifetime. Whether it's a romantic wedding ceremony, a joyful anniversary celebration, or a heartwarming family reunion, we strive to create moments that are filled with love, laughter, and happiness. Our goal is to exceed our clients' expectations and leave a lasting impression that they will treasure forever.</p>
+
+                            <h5 class="text-start card-title">Love for the Environment:</h5><p>
+                            We believe in spreading love not only to our clients but also to the environment. That's why we are committed to incorporating sustainable practices into our events whenever possible. From eco-friendly décor options to minimizing waste and supporting local vendors, we strive to make every event a celebration of love for both our clients and the planet.
+                            </p>
+                            At KHOT, love is more than just a theme—it's the driving force behind everything we do. Whether you're planning a wedding, a corporate event, or a social gathering, let us help you create a memorable experience that is infused with love, passion, and joy.
                         </div>
                     </div>                
                 </div>
@@ -150,9 +184,17 @@
                 </div>
                 <div class="col-sm-2">
                     <p class=""><b>Category</b></p>
+                    <a href="">Weddings and Celebrations</a>
+                    <a href="">Conferences and Conventions</a>
+                    <a href="">Performing Arts and Entertainment</a>
+                    <a href="">Community and Social Events</a>
                 </div>
                 <div class="col-sm-2">
-                <p class=""><b>Services</b></p>
+                    <p class=""><b>Services 24x7</b></p>
+                    <a href="">Venue Selection and Booking</a>
+                    <a href="">Event Planning and Coordination</a>
+                    <a href="">Budget Management</a>
+                    <a href="">Vendor Management</a>
                 </div>
                 <div class="col-sm-2">
                 <p class=""><b>Quick Link</b></p>
@@ -165,8 +207,10 @@
                 <div class="col-sm-2">
                     <p class=""><b>news later</b></p>
                     <p class="">subscribe to lastest media update</p>
-                    <input class="form-control form-control-sm" type="text" placeholder="email address" aria-label=".form-control-sm example">
-                    <button class="btn btn-success btn-sm">subscribe</button>
+                    <form>
+                        <input class="form-control form-control-sm" type="text" placeholder="email address" aria-label=".form-control-sm example">
+                        <button class="btn btn-success btn-sm mt-2">subscribe</button>
+                    </form>
                 </div>
                 <div class="col-sm-12">
                     <p class="text-center">copyright 2024</p>
