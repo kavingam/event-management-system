1c4fd4e5cfb1eccc1ab8ccdb6e2a0411ea0f9c84
kavingam <bhaitigam71@gmail.com>
2024-03-31 10:38:42 +0530

    update

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>KHOT | Event Management System </title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="assets/bootstrap-5.3.3-dist/css/bootstrap.min.css" />
     <link rel="stylesheet" href="assets/css/styles.css" />
     <style>
@@ -38,6 +39,49 @@
             border-radius: 5px;
             cursor: pointer;
         }    
+        .fa {
+            padding: 5px;
+            font-size: 14px;
+            width: 26px;
+            text-align: center;
+            text-decoration: none;
+            margin: 2px 0px;
+            }
+
+            .fa:hover {
+                opacity: 0.7;
+            }
+
+            .fa-facebook {
+            background: #3B5998;
+            color: white;
+            }
+
+            .fa-twitter {
+            background: #55ACEE;
+            color: white;
+            }
+
+            .fa-linkedin {
+            background: #007bb5;
+            color: white;
+            }
+
+            .fa-instagram {
+            background: #125688;
+            color: white;
+            }
+
+            .fa-pinterest {
+            background: #cb2027;
+            color: white;
+            }
+
+            .fa-snapchat-ghost {
+            background: #fffc00;
+            color: white;
+            text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+            }
     </style>
 </head>
 <body>
@@ -338,6 +382,12 @@
                 <div class="col-sm-2">
                     <h1 class="fs-6 ">KHOT Event Management</h1>
                     <p class="">Social Media</p>
+                    <a href="#" class="fa fa-facebook"></a>
+                    <a href="#" class="fa fa-twitter"></a>
+                    <a href="#" class="fa fa-instagram"></a>
+                    <a href="#" class="fa fa-snapchat-ghost"></a>
+                    <a href="#" class="fa fa-linkedin"></a>
+                    <a href="#" class="fa fa-pinterest"></a>
                 </div>
                 <div class="col-sm-2">
                     <p class=""><b>Category</b></p>
